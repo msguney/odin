a1dffa988a2f16fbbd99d8f6fcd34fa54f24c0e3
Seyda Güney <msguney@hotmail.co.uk>
2017-10-04 22:54:14 +0300

    Revert back to a previous changeset

--- Odin.Introduction.Console/Odin.Introduction.Console/Program.cs
+++ Odin.Introduction.Console/Odin.Introduction.Console/Program.cs
@@ -11,6 +11,8 @@ namespace Odin.Introduction.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine("Seyda");
+            System.Console.WriteLine("MuratCan");
+            System.Console.WriteLine("Tolga");
         }
     }
 }
